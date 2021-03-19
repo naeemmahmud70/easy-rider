c40e6a61f46d2260842681a3db2b053dae932b3c
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-03-19 16:53:34 +0600

    fourth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,47 +1,114 @@
 import React, { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 const Login = () => {
+    const [newUser, setNewUser] = useState(false);
     const [user, setUser] = useState({
         isSignedIn: false,
         email: '',
         password: '',
         name: '',
-        photo: '',
         error: '',
         success: false
     });
-    console.log(user.name, user.email, user.password)
+
     const handleBlur = (e) => {
-        let isFormValid;
+        let isFieldValid;
         if (e.target.name === 'email') {
-            isFormValid = /\S+@\S+\.\S+/.test(e.target.value)
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value)
 
         }
         if (e.target.name === 'password') {
             const isPasswordValid = e.target.value.length > 6;
             const passwordHasNumber = /\d{1}/.test(e.target.value)
-            isFormValid = isPasswordValid && passwordHasNumber
+            isFieldValid = isPasswordValid && passwordHasNumber
         }
         if (e.target.name === 'name') {
-            isFormValid = e.target.value.length > 4;
+            isFieldValid = e.target.value.length > 4;
         }
-        if (isFormValid) {
+        if (isFieldValid) {
             const newUserInfo = { ...user };
             newUserInfo[e.target.name] = e.target.value;
             setUser(newUserInfo);
         }
     }
+
+    const handleSubmit = (e) => {
+        if (newUser && user.email && user.password) {
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    const newUserInfo = { ...user }
+                    newUserInfo.error = '';
+                    newUserInfo.success = true;
+                    setUser(newUserInfo);
+                    updateUserName (user.name);
+                })
+                .catch((error) => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                });
+
+        }
+        if (!newUser && user.email && user.password) {
+            firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    const newUserInfo = { ...user }
+                    newUserInfo.error = '';
+                    newUserInfo.success = true;
+                    setUser(newUserInfo);
+                    console.log("updated name", res.user)
+                })
+                .catch((error) => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                });
+
+        }
+
+        e.preventDefault();
+    }
+
+    const updateUserName = name => {
+        const user = firebase.auth().currentUser;
+
+        user.updateProfile({
+            displayName: name,
+        }).then(function () {
+            console.log('updated')
+        }).catch(function (error) {
+            console.log(error)
+        });
+
+    }
+
     return (
         <div>
-            <form style={{ textAlign: 'center' }} >
-                <input name="name" type="text" onBlur={handleBlur} placeholder="enter your name" />
+            <input type="checkbox" onChange={() => setNewUser(!newUser)} name="" id="" />
+            <label htmlFor="newUser">New User SignUp</label>
+
+            <form onSubmit={handleSubmit} style={{ textAlign: 'center' }} >
+                {newUser && <input name="name" type="text" onBlur={handleBlur} placeholder="enter your name" />}
                 <br />
                 <input type="text" name="email" onBlur={handleBlur} placeholder="enter your email" required />
                 <br />
                 <input type="password" name="password" onBlur={handleBlur} placeholder="enter your password" required />
                 <br />
-                <input type="submit" />
+                <input type="submit" value={newUser ? 'Sign up' : 'Sign In'} />
             </form>
+
+            <p style={{ color: "red" }}>{user.error}</p>
+            {user.success && <p style={{ color: "green" }}>User {newUser ? 'Created' : 'Logged In'} Successfully</p>}
+
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2lYKYlOH_wwQaLBoAyh_zh8ours-ddsk",
+    authDomain: "rider-assignment.firebaseapp.com",
+    projectId: "rider-assignment",
+    storageBucket: "rider-assignment.appspot.com",
+    messagingSenderId: "347071531794",
+    appId: "1:347071531794:web:a0342155d8af12401d01cb"
+  };
+  export default firebaseConfig;
