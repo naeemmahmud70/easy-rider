364e9953b38f77e6c0ebecd1cea5908f8be5fbb2
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-03-20 10:06:20 +0600

    fiveth

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../Home/Header/Header';
+import Map from '../Map/Map';
+import SearchRider from '../SearchRider/SearchRider';
 
 const Destination = () => {
     const { type } = useParams();
-    console.log(type)
     return (
-        <div>
-            <div>
+        <div className="container">
+            <div className="row">
                 <Header></Header>
+                <div className='col-md-4'>
+                    <SearchRider type={type}></SearchRider>
+                </div>
+                <div className="col-md-8">
+                    <Map></Map>
+                </div>
             </div>
-            <h1>this is destination</h1>
         </div>
     );
 };
---
+++ src/components/SearchRider/SearchRider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import fakeData from '../../fakeData/data.json'
+import './SearchRider.css'
+
+const SearchRider = (props) => {
+    const type = props.type;
+    const item = fakeData.find(element => element.type === type);
+    const { img, icon, capacity, fare } = item;
+    console.log(img, icon, capacity, fare);
+    
+    return (
+        <div className="shadow ">
+            <div className="form-style p-3">
+                <input type="text" placeholder="from"/>
+                <input type="text" placeholder="to" />
+                <button className="button-style mt-2">Search</button>
+            </div>
+
+            <div className="search-style d-flex justify-content-around shadow">
+                <img className="type-img" src={img} alt="" />
+                <img className="icon-img" src={icon} alt="" />
+                <p>{capacity}</p>
+                <p>{fare}</p>
+            </div>
+
+            <div className="search-style d-flex justify-content-around shadow">
+                <img className="type-img" src={img} alt="" />
+                <img className="icon-img" src={icon} alt="" />
+                <p>{capacity}</p>
+                <p>{fare}</p>
+            </div>
+
+            <div className="search-style d-flex justify-content-around shadow">
+                <img className="type-img" src={img} alt="" />
+                <img className="icon-img" src={icon} alt="" />
+                <p>{capacity}</p>
+                <p>{fare}</p>
+            </div>
+
+
+            
+        </div>
+    );
+};
+
+export default SearchRider;
