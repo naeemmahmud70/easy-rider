bc64e0ebe96e0a07cd81e4a6c8402388305aa8d7
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-03-20 10:39:27 +0600

    sixth

--- src/components/SearchRider/SearchRider.js
+++ src/components/SearchRider/SearchRider.js
@@ -5,36 +5,41 @@ import './SearchRider.css'
 const SearchRider = (props) => {
     const type = props.type;
     const item = fakeData.find(element => element.type === type);
-    const { img, icon, capacity, fare } = item;
-    console.log(img, icon, capacity, fare);
+        const { img, icon, capacity, fare } = item;
+        console.log(img, icon, capacity, fare);
+
+    
+    const handelSearchButton =()=>{
+        console.log('search clicked')
+    }
     
     return (
         <div className="shadow ">
             <div className="form-style p-3">
                 <input type="text" placeholder="from"/>
                 <input type="text" placeholder="to" />
-                <button className="button-style mt-2">Search</button>
+                <button onClick={handelSearchButton} className="button-style mt-2">Search</button>
             </div>
 
             <div className="search-style d-flex justify-content-around shadow">
                 <img className="type-img" src={img} alt="" />
                 <img className="icon-img" src={icon} alt="" />
                 <p>{capacity}</p>
-                <p>{fare}</p>
+                <p>{"$"+fare}</p>
             </div>
 
             <div className="search-style d-flex justify-content-around shadow">
                 <img className="type-img" src={img} alt="" />
                 <img className="icon-img" src={icon} alt="" />
                 <p>{capacity}</p>
-                <p>{fare}</p>
+                <p>{"$"+fare}</p>
             </div>
 
             <div className="search-style d-flex justify-content-around shadow">
                 <img className="type-img" src={img} alt="" />
                 <img className="icon-img" src={icon} alt="" />
                 <p>{capacity}</p>
-                <p>{fare}</p>
+                <p>{"$"+fare}</p>
             </div>
 
 
